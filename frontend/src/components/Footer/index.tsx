93e448bfbb8482aac04bc2cc88fbe0fe2e59e8d6
DemissonAndrade <demissondsa@gmail.com>
2021-11-20 21:03:24 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light text-primary">App desenvolvido por <a href="https://github.com/demissonAndrade" target="_blank" rel="noreferrer">DemissonAndrade</a></p>
+                <p className="text-light text-primary"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+
+    );
+}
+export default Footer;
---
+++ frontend/src/components/NavBar/index.tsx
@@ -0,0 +1,13 @@
+
+const NavBar = () => {
+    return (
+        <div className="container-fluir">
+            <nav className="navbar navbar-expand-lg navbar-light bg-dark my-2 my-md-0 mr-md-3">
+                <div className="container ">
+                    <h2 className="text-primary">DevSuperior</h2>
+                </div>
+            </nav>
+        </div>
+    );
+}
+export default NavBar;
