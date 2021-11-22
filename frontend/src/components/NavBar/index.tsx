d1eddcee9c2af3197c960dd96c943c603687c6bd
DemissonAndrade <demissondsa@gmail.com>
2021-11-22 20:55:27 -0300

    Home, navigation

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
     return (
         <div className="container-fluir">
             <nav className="navbar navbar-expand-lg navbar-light bg-dark my-2 my-md-0 mr-md-3">
-                <div className="container ">
+                <div className="container-fluid ">
+                    <Link to= "/">
                     <h3 className="text-primary">DevSuperior</h3>
+                    </Link>
                 </div>
             </nav>
         </div>
