26dceec1f241c22cbdb79b10f1052886afaf40e7
DemissonAndrade <demissondsa@gmail.com>
2021-11-22 22:19:58 -0300

    BarChart integration

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -5,8 +5,8 @@ const NavBar = () => {
         <div className="container-fluir">
             <nav className="navbar navbar-expand-lg navbar-light bg-dark my-2 my-md-0 mr-md-3">
                 <div className="container-fluid ">
-                    <Link to= "/">
-                    <h3 className="text-primary">DevSuperior</h3>
+                    <Link to="/">
+                        <h3 className="text-primary">DevSuperior</h3>
                     </Link>
                 </div>
             </nav>
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -6,15 +6,15 @@ const Home = () => {
     return (
         <>
             <NavBar />
-            <div className="container">
+            <div className="container-fluid">
                 <div className="jumbotron">
                     <h1 className="display-6 text-primary">DSVendas</h1>
                     <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
                     <hr />
                     <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
-                <Link className="btn btn-primary btn-lg" to ="/dashboard">
-                    Acessar Dashboard
-                </Link>
+                    <Link className="btn btn-primary btn-lg" to="/dashboard">
+                        Acessar Dashboard
+                    </Link>
                 </div>
             </div>
             <Footer />
